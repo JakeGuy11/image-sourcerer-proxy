7406a477b126e4bcd90fc516fd48814fc08afc9a
Jake <Jake_Guy_11@protonmail.ch>
2021-08-14 19:02:46 -0400

    added custom header code

--- web/custom_header.php
+++ web/custom_header.php
@@ -3,5 +3,19 @@
     $header_content= $_GET['content'];
     $image_url = $_GET['src'];
 
-    echo "$header_name should be $header_content for retrieving $image_url";
+    $curl_h = curl_init($image_url);
+    curl_setopt($curl_h, CURLOPT_HTTPHEADER, array(
+        'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Encoding: gzip, deflate',
+        'Dnt: 1',
+        'Upgrade-Insecure-Requests: 1',
+        "$header_name: $header_content"
+    ));
+    curl_setopt($curl_h, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($curl_h);
+    curl_close($curl_h);
+    
+    header('Content-Type: image/png');
+    echo $response;
+    die();
 ?>

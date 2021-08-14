dd5f0953a3737981bbcae3d2119f2e95fe811a18
Jake <Jake_Guy_11@protonmail.ch>
2021-08-14 12:54:13 -0400

    added custom_header file

---
+++ web/custom_header.php
@@ -0,0 +1,7 @@
+<?php
+    $header_name = $_GET['name'];
+    $header_content= $_GET['content'];
+    $image_url = $_GET['src'];
+
+    echo "$header_name should be $header_content for retrieving $image_url";
+?>
